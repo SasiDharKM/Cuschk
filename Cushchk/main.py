b90a7ab821f4d3c60b16ccd31a861b8245095c46
SasiDharKM <sasidhar.km97@gmail.com>
2017-06-23 14:45:17 +0530

    Added comments

--- Cushchk/main.py
+++ Cushchk/main.py
@@ -1,16 +1,16 @@
 import urllib.request
 
 def read_file():
-	text = open("../Test/Test1")
+	text = open("../Test/Test1")#defining a path
 	contents = text.read()
 	print(contents)
 	text.close()
 	cuschk(contents)
 
 def cuschk(text):
-	con = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text.replace(' ','+'))
+	con = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text.replace(' ','+'))#replacing <spc> with + to prevent bad requests
 	output = con.read()
-	print(output)
+	#print(output)
 	con.close()
 
 
