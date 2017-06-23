b1f4d92102c89231408b478dbd23b55555fe1936
SasiDharKM <sasidhar.km97@gmail.com>
2017-06-23 14:56:29 +0530

    modified the output

--- Cushchk/main.py
+++ Cushchk/main.py
@@ -3,15 +3,20 @@ import urllib.request
 def read_file():
 	text = open("../Test/Test1")#defining a path
 	contents = text.read()
-	print(contents)
 	text.close()
 	cuschk(contents)
 
 def cuschk(text):
 	con = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text.replace(' ','+'))#replacing <spc> with + to prevent bad requests
-	output = con.read()
+	bytes_output = con.read()
+	output = bytes_output.decode("utf-8")
 	#print(output)
 	con.close()
-
+	if 'true' in output:
+		print("Are you sure you want to use PROFANE Words...?")
+	elif 'false' in output:
+		print("The text appears to be FREE from curse words")
+	else:
+		print("Scan of document is improper")
 
 read_file()
